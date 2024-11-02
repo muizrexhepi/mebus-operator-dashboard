b54aae03e3aa4b9a3b6a4a90b7f7737b9498a713
etnik2002 <etnikz2002@gmail.com>
2024-11-02 18:48:04 +0100

    smetkacioni agjenci

--- components/forms/create-agency-form.tsx
+++ components/forms/create-agency-form.tsx
@@ -1,98 +1,99 @@
 "use client"
 
-import React, { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import * as z from 'zod';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form';
-import { Loader } from 'lucide-react';
-import { FormError } from '@/components/form-error';
-import { FormSuccess } from '@/components/form-success';
-import { createAgency } from '@/actions/agency';
-import { useUser } from '@/context/user';
+// import React, { useState } from 'react';
+// import { useForm } from 'react-hook-form';
+// import { zodResolver } from '@hookform/resolvers/zod';
+// import * as z from 'zod';
+// import { Button } from '@/components/ui/button';
+// import { Input } from '@/components/ui/input';
+// import {
+//   Form,
+//   FormControl,
+//   FormField,
+//   FormItem,
+//   FormLabel,
+//   FormMessage,
+// } from '@/components/ui/form';
+// import { Loader } from 'lucide-react';
+// import { FormError } from '@/components/form-error';
+// import { FormSuccess } from '@/components/form-success';
+// import { createAgency } from '@/actions/agency';
+// import { useUser } from '@/context/user';
 
-const agencySchema = z.object({
-  name: z.string().min(1, 'Name is required'),
-  email: z.string().email('Invalid email address'),
-  password: z.string().min(6, 'Password must be at least 6 characters'),
-  address: z.object({
-    city: z.string().optional(),
-    country: z.string().optional(),
-    street: z.string().optional(),
-  }),
-  contact: z.object({
-    phone: z.string().optional(),
-    contact_email: z.string().email('Invalid contact email').optional(),
-  }),
-  financial_data: z.object({
-    // percentage: z.number().min(0).max(100).optional(),
-    total_sales: z.number().min(0).optional(),
-    profit: z.number().min(0).optional(),
-    debt: z.number().min(0).optional(),
-  }),
-  company_metadata: z.object({
-    tax_number: z.string().optional(),
-    registration_number: z.string().optional(),
-    name: z.string().optional(),
-    email: z.string().email('Invalid company email').optional(),
-    phone: z.string().optional(),
-    country: z.string().optional(),
-  }),
-});
+// const agencySchema = z.object({
+//   name: z.string().min(1, 'Name is required'),
+//   email: z.string().email('Invalid email address'),
+//   password: z.string().min(6, 'Password must be at least 6 characters'),
+//   address: z.object({
+//     city: z.string().optional(),
+//     country: z.string().optional(),
+//     street: z.string().optional(),
+//   }),
+//   contact: z.object({
+//     phone: z.string().optional(),
+//     contact_email: z.string().email('Invalid contact email').optional(),
+//   }),
+//   financial_data: z.object({
+//     // percentage: z.number().min(0).max(100).optional(),
+//     total_sales: z.number().min(0).optional(),
+//     profit: z.number().min(0).optional(),
+//     debt: z.number().min(0).optional(),
+//   }),
+//   company_metadata: z.object({
+//     tax_number: z.string().optional(),
+//     registration_number: z.string().optional(),
+//     name: z.string().optional(),
+//     email: z.string().email('Invalid company email').optional(),
+//     phone: z.string().optional(),
+//     country: z.string().optional(),
+//   }),
+// });
 
-export default function AgencyForm() {
-  const [error, setError] = useState('');
-  const [success, setSuccess] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-  const {user} = useUser();
+// export default function AgencyForm() {
+//   const [error, setError] = useState('');
+//   const [success, setSuccess] = useState('');
+//   const [isLoading, setIsLoading] = useState(false);
+//   const {user} = useUser();
 
 
-  const form = useForm({
-    resolver: zodResolver(agencySchema),
-    defaultValues: {
-      name: '',
-      email: '',
-      password: '',
-      address: { city: '', country: '', street: '' },
-      contact: { phone: '', contact_email: '' },
-      financial_data: { percentage: 0, total_sales: 0, profit: 0, debt: 0 },
-      company_metadata: {
-        tax_number: '',
-        registration_number: '',
-        name: '',
-        email: '',
-        phone: '',
-        country: '',
-      },
-    },
-  });
+//   const form = useForm({
+//     resolver: zodResolver(agencySchema),
+//     defaultValues: {
+//       name: '',
+//       email: '',
+//       password: '',
+//       address: { city: '', country: '', street: '' },
+//       contact: { phone: '', contact_email: '' },
+//       financial_data: { percentage: 0, total_sales: 0, profit: 0, debt: 0 },
+//       company_metadata: {
+//         tax_number: '',
+//         registration_number: '',
+//         name: '',
+//         email: '',
+//         phone: '',
+//         country: '',
+//       },
+//     },
+//   });
 
-  const onSubmit = async (values: z.infer<typeof agencySchema>) => {
-    setIsLoading(true);
-    try {
-      const message = await createAgency(values, user?.$id);
-      setSuccess(message);
-      console.log({ message });
-    } catch (error) {
-      console.error(error);
-      setError("An error occurred while submitting the form.");
-    } finally {
-      setIsLoading(false);
-    }
-  };
+//   const onSubmit = async (values: z.infer<typeof agencySchema>) => {
+//     setIsLoading(true);
+//     try {
+//       const message = await createAgency(values, user?.$id);
+//       setSuccess(message);
+//       console.log({ message });
+//     } catch (error) {
+//       console.error(error);
+//       setError("An error occurred while submitting the form.");
+//     } finally {
+//       setIsLoading(false);
+//     }
+//   };
 
   return (
     <div className="space-y-6">
-      <Form {...form}>
+      <h1>pse skrijojn mo</h1>
+      {/* <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
           <FormField
             control={form.control}
@@ -319,7 +320,7 @@ export default function AgencyForm() {
             )}
           </Button>
         </form>
-      </Form>
+      </Form> */}
     </div>
   );
 }
